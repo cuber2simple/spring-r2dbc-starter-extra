e0480fd154d77c9a13416b76d55df326275b2917
cuber <cuber.carrots@gmail.com>
2022-10-11 11:10:56 +0800

    提交代码

---
+++ src/main/java/org/cuber2simple/constants/REConstants.java
@@ -0,0 +1,6 @@
+package org.cuber2simple.constants;
+
+public class REConstants {
+
+    public static final String NVL = "nvl";
+}
---
+++ src/main/java/org/cuber2simple/utils/StringUtil.java
@@ -0,0 +1,4 @@
+package org.cuber2simple.utils;
+
+public class StringUtil {
+}
